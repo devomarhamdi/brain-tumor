42af6bede4726943138074bec2059657a07e319a
Omar <devomarhamdi@gmail.com>
2024-02-23 21:42:10 +0200

    test 2

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def predict():
         if file:
             img = file.read()
             # prediction = img_pred(img)
-            return jsonify({'prediction': img})
+            return jsonify({'prediction': 'img'})
         else:
             return jsonify({'error': 'No image uploaded'})
 
